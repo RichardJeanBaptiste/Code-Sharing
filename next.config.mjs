ef3e8a93074c4882dc59b9a38f68e3b4290fcf3c
Richard Jean-Baptiste <Richinbk1@gmail.com>
2024-02-16 22:20:41 -0500

    added monaco-editor dependency/ changed webpack/next config

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,9 @@ const nextConfig = {
         // Add MonacoWebpackPlugin only during client-side build
         if (!isServer) {
           config.plugins.push(
+            new HtmlWebpackPlugin({
+              template: path.join(__dirname, "src/index.html"), // index html file
+            }),
             new MonacoWebpackPlugin({
               // Options for the plugin
               languages: ['json'], // Add the languages you want to support
