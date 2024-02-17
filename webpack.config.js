7e2835a55dcfebe5f6dd8464f633b373d2b21505
Richard Jean-Baptiste <Richinbk1@gmail.com>
2024-02-16 22:22:09 -0500

    removed html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,6 @@
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 module.exports = {
   plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, "src/index.html"), // index html file
-    }),
     new MonacoWebpackPlugin({
       // available options are documented at https://github.com/microsoft/monaco-editor/blob/main/webpack-plugin/README.md#options
       languages: ['json']
